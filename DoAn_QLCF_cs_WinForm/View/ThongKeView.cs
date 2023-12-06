1ba96e025ec264c00ce8fc6cc2c8166cbe893b79
Baroneee <pcbao159@gmail.com>
2023-12-06 13:48:40 +0700

    thongkeView

--- DoAn_QLCF_cs_WinForm/View/ThongKeView.cs
+++ DoAn_QLCF_cs_WinForm/View/ThongKeView.cs
@@ -11,34 +11,34 @@ using System.Windows.Forms;
 
 namespace DoAn_QLCF_cs_WinForm.View
 {
-	public partial class ThongKeView : Form, IThongKeView
-	{
-		public ThongKeView()
-		{
-			InitializeComponent();
-		}
+    public partial class ThongKeView : Form, IThongKeView
+    {
+        public ThongKeView()
+        {
+            InitializeComponent();
+        }
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-		private static ThongKeView instance;
+        private static ThongKeView instance;
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-		public static IThongKeView GetInstance(Form parentContainer)
-		{
+        public static IThongKeView GetInstance(Form parentContainer)
+        {
 
-			if (instance == null || instance.IsDisposed)
-			{
-				instance = new ThongKeView();
-				instance.TopLevel = false;
-				parentContainer.Controls.Add(instance);
-				instance.Dock = DockStyle.Fill;
-			}
-			else
-			{
-				if (instance.WindowState == FormWindowState.Minimized)
-					instance.WindowState = FormWindowState.Normal;
+            if (instance == null || instance.IsDisposed)
+            {
+                instance = new ThongKeView();
+                instance.TopLevel = false;
+                parentContainer.Controls.Add(instance);
+                instance.Dock = DockStyle.Fill;
+            }
+            else
+            {
+                if (instance.WindowState == FormWindowState.Minimized)
+                    instance.WindowState = FormWindowState.Normal;
 
-			}
-			instance.Show();
-			return instance;
-		}
-	}
+            }
+            instance.Show();
+            return instance;
+        }
+    }
 }
