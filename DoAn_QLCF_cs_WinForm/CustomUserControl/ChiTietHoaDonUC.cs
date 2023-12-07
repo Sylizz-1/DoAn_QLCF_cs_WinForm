585d5ceb9eaacf6fa5fc9dc19d456cf233ed5ee1
nguyen huy khiem <nguyenhuykhiem2111@gmail.com>
2023-12-08 02:09:02 +0700

    repair but not sure

--- DoAn_QLCF_cs_WinForm/CustomUserControl/ChiTietHoaDonUC.cs
+++ DoAn_QLCF_cs_WinForm/CustomUserControl/ChiTietHoaDonUC.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace DoAn_QLCF_cs_WinForm.View
+namespace DoAn_QLCF_cs_WinForm.CustomUserControl
 {
 	public partial class ChiTietHoaDonUC : UserControl
 	{
